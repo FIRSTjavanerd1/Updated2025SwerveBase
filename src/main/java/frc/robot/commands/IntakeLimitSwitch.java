170cabb90960a71e6b03d18423d1aa5bdedb90f0
FRCTeam3458 <robotics3458@gmail.com>
2025-03-05 17:43:11 -0500

    Attempting to make the pivot go up on limit switch press

--- src/main/java/frc/robot/commands/IntakeLimitSwitch.java
+++ src/main/java/frc/robot/commands/IntakeLimitSwitch.java
@@ -2,14 +2,17 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.Pivot;
 
 public class IntakeLimitSwitch extends Command{
 
     private final Intake intake;
+    private final Pivot pivot;
 
-    public IntakeLimitSwitch(Intake intake) {
+    public IntakeLimitSwitch(Intake intake, Pivot pivot) {
         this.intake = intake;
-        addRequirements(intake);
+        this.pivot = pivot;
+        addRequirements(intake,pivot);
     }
 
     @Override
@@ -18,6 +21,7 @@ public class IntakeLimitSwitch extends Command{
             intake.setIntakeSpeed(0.8); 
         } else {
             intake.setIntakeSpeed(0); 
+            pivot.pivotUp();
         }
         
     }
